0fb1450e5f8f67ca621129af4e1b440cd33db9a5
jenniferot <56264114+Jenniferot@users.noreply.github.com>
2020-06-17 15:31:17 +0200

    added watchlist

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-const authenticateUser = async ( req, res, next ) => {
+const authenticateUser = async (req, res, next) => {
   try {
     const user = await User.findOne({
       accessToken: req.header('Authorization')
@@ -29,37 +29,37 @@ const authenticateUser = async ( req, res, next ) => {
       res.status(403).json({ authorized: false })
     }
   } catch (err) {
-    res.status(403).json({ message: "Access token missing or incorrect", errors: err})
+    res.status(403).json({ message: "Access token missing or incorrect", errors: err })
   }
 }
 
 app.post('/users', async (req, res) => {
   try {
     const { name, email, password } = req.body
-    const duplicateEmail = await User.findOne({ email: email}).collation({ locale: "en_US", strength: 1 })
-    const duplicateUser = await User.findOne({ name: name}).collation({ locale: "en_US", strength: 1 })
-    
+    const duplicateEmail = await User.findOne({ email: email }).collation({ locale: "en_US", strength: 1 })
+    const duplicateUser = await User.findOne({ name: name }).collation({ locale: "en_US", strength: 1 })
+
     if (duplicateEmail) {
-      throw {code: 12000}
+      throw { code: 12000 }
     }
 
     if (duplicateUser) {
-      throw {code: 11000}
+      throw { code: 11000 }
     }
-    
+
     const hash = bcrypt.hashSync(password, 10);
-    const user = new User({name, email, password: hash})
+    const user = new User({ name, email, password: hash })
     const saved = await user.save()
-    
-    res.status(201).json({userId: saved._id, accessToken: saved.accessToken})
+
+    res.status(201).json({ userId: saved._id, accessToken: saved.accessToken })
   } catch (err) {
     switch (err.code) {
-      case 11000: 
+      case 11000:
         res.status(404).json({ message: "User already exists!", errors: err })
-      break;
+        break;
       case 12000:
-        res.status(404).json({ message: "Email already registered!", errors: err})
-      break;
+        res.status(404).json({ message: "Email already registered!", errors: err })
+        break;
       default: res.status(404).json({ message: "Could not create user", errors: err })
     }
   }
@@ -67,7 +67,7 @@ app.post('/users', async (req, res) => {
 
 app.post('/users/:id', authenticateUser)
 app.post('/users/:id', async (req, res) => {
-  res.status(201).json({ name: req.user.name, userId: req.user._id, authorized: true})
+  res.status(201).json({ name: req.user.name, userId: req.user._id, authorized: true })
 })
 
 app.put('/users/:userId/watchlist', authenticateUser)
@@ -76,34 +76,43 @@ app.put('/users/:userId/watchlist', async (req, res) => {
     const { userId } = req.params
     const { title, showId } = req.body
 
-    const duplicateShow = await User.findOne({ _id: userId,  "watchlist.showId" : showId })
-       
+    const duplicateShow = await User.findOne({ _id: userId, "watchlist.showId": showId })
+
     if (duplicateShow) {
       throw 'Show already added!'
     }
 
     const showItem = { "title": title, "showId": showId }
 
-    await User.updateOne({ _id: userId}, {$push: { watchlist: showItem }})
+    await User.updateOne({ _id: userId }, { $push: { watchlist: showItem } })
 
-    res.status(200).json({ message: "Watchlist updated! "})
+    res.status(200).json({ message: "Watchlist updated! " })
   } catch (err) {
-    res.status(201).json({ message: "Could not update watchlist!", errors: err})
+    res.status(201).json({ message: "Could not update watchlist!", errors: err })
   }
+})
+
+app.get('/users/:userId/watchlist', authenticateUser)
+app.get('/users/:userId/watchlist', async (req, res) => {
+  const { userId } = req.params
+
+  const user = await User.findOne({ _id: userId })
+
+  res.status(200).json({ watchlist: user.watchlist })
 })
 
 app.post('/sessions', async (req, res) => {
   try {
     const { name, password } = req.body
     const user = await User.findOne({ name: name }).collation({ locale: "en_US", strength: 1 })
-    
+
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(202).json({userId: user._id, userName: user.name, accessToken: user.accessToken})
+      res.status(202).json({ userId: user._id, userName: user.name, accessToken: user.accessToken })
     } else {
       res.status(404).json({ notFound: true })
     }
   } catch (err) {
-    res.status(404).json({  notFound: true })
+    res.status(404).json({ notFound: true })
   }
 })
 
